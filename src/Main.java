857f98cf2b7891495996947aaaf4d75952133a5e
Abou <bou1734@gmail.com>
2024-09-24 12:11:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.sql.SQLOutput;
+
+public class Main
+{
+   public static void main(String[] args) {
+       int intOperandA = 23;
+       int intOperandB = 14;
+
+       int intSum = 0;
+       int intProduct = 0;
+       int intQuotient = 0;
+       int intDifference = 0;
+       int intModulo = 0;
+
+       intSum = intOperandA + intOperandB;
+       System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+       System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+       System.out.println();
+
+
+
+       double doubleOperandA = 21.23;
+       double doubleOperandB = 10.64;
+
+       double doubleSum = 0;
+       double doubleProduct = 0;
+       double doubleQuotient = 0;
+       double doubleDifference = 0;
+
+       doubleSum = doubleOperandA + doubleOperandB;
+
+       // complete this for doubles
+
+
+
+   }
+}
